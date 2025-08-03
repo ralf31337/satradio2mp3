4e8fbd0da0b6141ff0834fcb87fc899e8d302fdf
ra31337 <github@ralf.siedow.de>
2025-08-03 20:41:19 +0200

    Added header inspired by official station

--- radio_server.py
+++ radio_server.py
@@ -7,6 +7,7 @@ PORT = int(os.environ.get("STREAM_PORT", "8000"))
 STREAM_URL = os.environ.get("STREAM_URL")
 AUDIO_BITRATE = os.environ.get("AUDIO_BITRATE", "192k")
 AUDIO_RATE = os.environ.get("AUDIO_RATE", "48000")
+ICY_BITRATE = AUDIO_BITRATE.replace("k", "")
 
 class RadioHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,7 +16,14 @@ class RadioHandler(BaseHTTPRequestHandler):
             return
 
         self.send_response(200)
-        self.send_header('Content-Type', 'audio/mpeg')
+        self.send_header("Content-Type", "audio/mpeg")
+        self.send_header("icy-br", AUDIO_RATE)
+        self.send_header("ice-audio-info", f"ice-bitrate={ICY_BITRATE};ice-channels=2;ice-samplerate={AUDIO_RATE}")
+        self.send_header("icy-name", "SATRadio")
+        self.send_header("Accept-Ranges", "none")
+        self.send_header("Cache-Control", "no-cache, no-store")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Connection", "close")
         self.end_headers()
 
         ffmpeg_cmd = [
@@ -41,12 +49,15 @@ class RadioHandler(BaseHTTPRequestHandler):
                 proc.terminate()
     
     def do_HEAD(self):
-        print(f"HEAD request from: {self.client_address}, User-Agent: {self.headers.get('User-Agent')}")
         self.send_response(200)
-        self.send_header('Content-Type', 'audio/mpeg')
-        self.send_header('Content-Length', '999999999')  # Dummy large value
-        self.send_header('Accept-Ranges', 'bytes')
-        self.send_header('Connection', 'close')
+        self.send_header("Content-Type", "audio/mpeg")
+        self.send_header("icy-br", AUDIO_RATE)
+        self.send_header("ice-audio-info", f"ice-bitrate={ICY_BITRATE};ice-channels=2;ice-samplerate={AUDIO_RATE}")
+        self.send_header("icy-name", "SATRadio")
+        self.send_header("Accept-Ranges", "none")
+        self.send_header("Cache-Control", "no-cache, no-store")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Connection", "close")
         self.end_headers()
 
 def run_server():
